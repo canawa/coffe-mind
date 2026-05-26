704a71e1a1625fd12efe051478da4db55057e490
den <faceitlolaw@gmail.com>
2026-05-26 21:45:56 +0300

    udapte

--- databases/config.py
+++ databases/config.py
@@ -1,5 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # подгружаем env
 
+load_dotenv('../envs/.env') # подгружаем env
+
+class Settings:
+    def __init__(self):
+        self.user = os.getenv('DB_USER')
+        self.db_host = os.getenv('DB_HOST')
+        self.db_password = os.getenv('DB_PASSWORD')
+        self.db_name = os.getenv('db_name')
+        self.db_port = os.getenv('db_port')
+
+settings = Settings()
+print(settings.__dict__)
